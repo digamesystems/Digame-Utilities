36789bc0b9acbccc671cc28b99517705ea25bd65
John Price <john.price@digamesystems.com>
2022-08-20 12:37:11 -0700

    Significant re-factoring of digameDisplay.h -- Will require changes to existing code bases in: digameVehicleCounter, digameDirectionalVehicleCounter, digameLoRaBaseStation, digameShuttleCounter etc.

--- digameDebug.h
+++ digameDebug.h
@@ -5,12 +5,12 @@
 #define SHOW_DEBUG
 
 // Define where debug output will be printed.
-#define DEBUG_PRINTER Serial
+#define DEBUG_UART Serial
 
 // Setup debug printing macros.
 #ifdef SHOW_DEBUG
-  #define DEBUG_PRINT(...) { DEBUG_PRINTER.print(__VA_ARGS__); }
-  #define DEBUG_PRINTLN(...) { DEBUG_PRINTER.println(__VA_ARGS__); }
+  #define DEBUG_PRINT(...) { DEBUG_UART.print(__VA_ARGS__); }
+  #define DEBUG_PRINTLN(...) { DEBUG_UART.println(__VA_ARGS__); }
 #else
   #define DEBUG_PRINT(...) {}
   #define DEBUG_PRINTLN(...) {}
